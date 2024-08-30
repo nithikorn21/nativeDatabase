bd449a59ec691ad4b37c10d07480e6012cbff150
nithikorn21 <nithikorn.int@gmail.com>
2024-08-30 10:58:47 +0700

    fist commit
    add project

---
+++ src/main/java/com/db/nativeDatabase/NativeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.db.nativeDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/nativeDatabase/NativeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.nativeDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
