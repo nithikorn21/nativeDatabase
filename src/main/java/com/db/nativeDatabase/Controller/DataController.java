8a5ae4d1b30f9e4d174cae0efae9b45741354d95
nithikorn21 <nithikorn.int@gmail.com>
2024-08-30 16:06:32 +0700

    standard api

---
+++ src/main/java/com/db/nativeDatabase/Controller/DataController.java
@@ -0,0 +1,46 @@
+package com.db.nativeDatabase.Controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.db.nativeDatabase.Entity.Book;
+import com.db.nativeDatabase.Repository.BookRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@RestController
+public class DataController {
+
+    private final BookRepository data;
+
+    public DataController(BookRepository data) {
+        this.data = data;
+    }
+    
+   @GetMapping("/findAllData")
+   public List<Book> getMethodName() {
+       return data.findAll();
+   }
+   
+//    @PostMapping("/findDataID")
+//    public Optional<Book> postMethodName(@RequestBody Map<String, Object> payload) {
+//    Long id = Long.valueOf(payload.get("id").toString());
+//        return data.findById(id);
+//    }
+
+    @PostMapping("/findDataID")
+    public Optional<Book> postMethodName(@RequestParam long id) {
+        return data.findById(id);
+    }
+
+   
+    
+}
---
+++ src/main/java/com/db/nativeDatabase/Repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.db.nativeDatabase.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.db.nativeDatabase.Entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
